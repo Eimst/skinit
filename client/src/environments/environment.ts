f2a694c77dc57153177b781e62cf2e04da73b6a4
Eimst <sutkaitisee@gmail.com>
2024-11-15 11:31:33 +0200

    Update

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apUrl: 'api/',
   hubUrl: 'hub/notifications',
-  stripePublicKey: "pk_test_51QIaldRx8T80CRSlGpuzlGG4l0WLrY4qZzwTcRVvhgQTRP4OaSYMvpgUcs4TQ5HHcxwscFWxr8Yq2AYkSWfY0SbL00qb8KQlD9"
+  stripePublicKey: "pk_test_51QL9XeDVgkAe2NdF0bXY0hE7Qw1hOhVqrtDHdEC8KuuOYD9ZH8Um4ascb0bGsWNSIq387QRfwL7H1foAQ90u7r1o00iSsOyw3D"
 };
